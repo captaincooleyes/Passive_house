fcbdcdad6481122c851a9dcd0ea5daef270dc8b6
Igor <captaincooleyes@users.noreply.github.com>
2022-12-26 12:24:44 +0200

    init project

---
+++ src/main/java/com/example/passivehouse/PassivehouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.passivehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassivehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassivehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/passivehouse/PassivehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.passivehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassivehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
