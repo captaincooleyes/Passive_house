c921edcbae6585f878c1539e2ad1b3b1e4082375
Igor <captaincooleyes@users.noreply.github.com>
2022-12-26 13:07:11 +0200

    fix name for PassiveHouseApplication class

--- src/main/java/com/example/passivehouse/PassiveHouseApplication.java
+++ src/main/java/com/example/passivehouse/PassiveHouseApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PassivehouseApplication {
+public class PassiveHouseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PassivehouseApplication.class, args);
+		SpringApplication.run(PassiveHouseApplication.class, args);
 	}
 
 }
