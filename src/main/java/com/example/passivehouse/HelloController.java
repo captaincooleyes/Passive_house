f582742ee1f969fd00510a9e10033eb3f7dda6c4
Igor <captaincooleyes@users.noreply.github.com>
2022-12-26 13:06:19 +0200

    add Hello World endpoint

---
+++ src/main/java/com/example/passivehouse/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.passivehouse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index(){
+        return "Greetings from...";
+    }
+}
