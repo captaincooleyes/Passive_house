2264d7592c738c700d92da3b1c3abefe97a72626
Igor <captaincooleyes@users.noreply.github.com>
2022-12-28 12:02:54 +0200

    add zoneId to URL

--- src/main/java/com/example/passivehouse/controller/WallController.java
+++ src/main/java/com/example/passivehouse/controller/WallController.java
@@ -1,12 +1,13 @@
 package com.example.passivehouse.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WallController {
-    @GetMapping("/calculate")
-    public double calcCoeff() {
-        return 42.0;
+    @GetMapping("/calculate/{zoneId}")
+    public double calcCoeff(@PathVariable Integer zoneId) {
+        return zoneId;
     }
 }
